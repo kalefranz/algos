6b0aaa5aded8b012c1f39ae519cff60f4008cf4c
Kale Franz <kfranz@23andme.com>
2022-04-05 10:10:33 -0500

    finished 4

--- leetcode/lc/numbered/4.py
+++ leetcode/lc/numbered/4.py
@@ -33,32 +33,15 @@ def bounds(arry, pivot):
     return lower_val, upper_val
 
 
-def median(p1, a1, a2):
-    LEN = len(a1) + len(a2)
-    EVEN = LEN % 2 == 0
-    PIVOT = LEN // 2
-    if EVEN:
-        PIVOT -= 1
-    p2 = PIVOT - p1
-
-    a1l, a1u = bounds(a1, p1)
-    a2l, a2u = bounds(a2, p2)
-
-    len_balance = (len(a1[:p1]) + len(a2[:p2])) - (len(a1[p1:]) + len(a2[p2:]))
-    lower_max = max(a1l, a2l)
-    upper_min = min(a1u, a2u)
-
-    p12, lmum = f"{p1},{p2}", f"{lower_max},{upper_min}"
-    lowers, uppers = f"({a1l},{a2l})", f"({a1u},{a2u})"
-    return f"{p12:>8} {len_balance:>6} {lowers:>14} {uppers:>14} {lmum:>12}"
-
-
-a12 = [0,1,2,3,8], [6,10,11,12]  # odd
-# a12 = [0,0,0,0,1,2,3,8], [6,10,11,12,12,12,12]  # odd
-# a12 = [1,2,5,5,5], [5,5,5,6,7,8]  # odd
-ltot = len(a12[0]) + len(a12[1])
-for q in range(-ltot, ltot):
-    print(median(q, *a12))
+def median(arry):
+    if len(arry) == 1:
+        return arry[0]
+    if len(arry) % 2 == 0:
+        p = len(arry) // 2
+        return (arry[p-1] + arry[p]) / 2
+    else:
+        p = len(arry) // 2
+        return arry[p]
 
 
 class Solution:
@@ -66,6 +49,8 @@ class Solution:
         def control_vals(piv):
             p1 = piv
             p2 = PIVOT - p1
+            if not ODD:
+                p2 += 1
 
             a1l, a1u = bounds(a1, p1)
             a2l, a2u = bounds(a2, p2)
@@ -78,30 +63,37 @@ class Solution:
 
             return len_balance, lower_max, upper_min, next_piv_dxn
 
-
         LTOT = len(a1) + len(a2)  # total length
+        if len(a1) == 0:
+            from statistics import median
+            return median(a2)
+        if len(a2) == 0:
+            from statistics import median
+            return median(a1)
         ODD = LTOT % 2  # even_odd
         PIVOT = LTOT // 2  # piv1 + piv2
-        if not ODD:
+        if ODD:
+            LEN_BALANCE = 1
+        else:
             PIVOT -= 1
+            LEN_BALANCE = 2
 
         pivot = int(LTOT / 4)
         len_balance, lower_max, upper_min, next_piv_dxn = control_vals(pivot)
 
         cnt = 0
-        while abs(len_balance) > 1 or lower_max > upper_min:
-            print(pivot, next_piv_dxn, abs(len_balance) > 1, lower_max > upper_min)
+        while abs(len_balance) > LEN_BALANCE or lower_max > upper_min:
+            # print(pivot, next_piv_dxn, len_balance, lower_max > upper_min)
             cnt += 1
-            if cnt > 12:
-                print(f"FAILLLLLLLLL {cnt}")
-                break
             if next_piv_dxn == "up":
-                pivot = pivot + (LTOT - pivot) // 2
+                # pivot = pivot + (LTOT - pivot) // 2
+                pivot += 1
             else:
-                pivot = pivot - pivot // 2
+                # pivot = pivot - pivot // 2
+                pivot -= 1
             len_balance, lower_max, upper_min, next_piv_dxn = control_vals(pivot)
 
-        return -1
+        return upper_min if ODD else (upper_min + lower_max) / 2
 
 
 data = load_json(int(os.path.basename(__file__)[:-3]))
@@ -109,8 +101,12 @@ CASES = (
     (6, [0,1,2,3,8], [6,10,11,12]),  # odd LTOT
     (6, [0,0,0,0,1,2,3,8], [6,10,11,12,12,12,12]),  # odd LTOT
     (5, [1,2,5,5,5], [5,5,5,6,7,8]),  # odd LTOT
-    (2.0, [1,3], [2]),
-    (2.5, [1,2], [3,4]),
+    (2.0, [1,3], [2]),  # odd LTOT
+    (2.5, [1,2], [3,4]),  # even LTOT
+    (7.0, [0,0,0,1,2,3,8], [6,10,11,12,12,12,12]),  # even LTOT
+    (1, [1], []),
+    (1, [], [1]),
+    (2.5, [], [2,3])
 )
 TEST_CALL = getattr(Solution(), list(Solution.__dict__.keys())[1])
 def test(*test_nums):
@@ -135,4 +131,4 @@ def test(*test_nums):
 
 if __name__ == "__main__":
     import sys
-    sys.exit(test(0))
+    sys.exit(test())
