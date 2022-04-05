6017d43a2eff70d3c61d384fdc136542afe1eb53
Kale Franz <kfranz@23andme.com>
2022-04-05 09:03:14 -0500

    11

---
+++ leetcode/lc/numbered/11.py
@@ -0,0 +1,64 @@
+"""
+11. Container With Most Water
+Medium
+
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+https://leetcode.com/problems/container-with-most-water/
+
+"""
+from collections import deque
+import os.path
+from typing import *
+
+from lc.numbered import load_json
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height) - 1
+        max_seen = 0
+        while left < right:
+            max_seen = max(max_seen, (right - left) * min(height[left], height[right]))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return max_seen
+
+
+data = load_json(int(os.path.basename(__file__)[:-3]))
+CASES = (
+    (49, [1,8,6,2,5,4,8,3,7]),
+    (1, [1,1]),
+)
+TEST_CALL = getattr(Solution(), list(Solution.__dict__.keys())[1])
+def test(*test_nums):
+    cases = test_nums and [CASES[num] for num in test_nums] or CASES
+
+    failed = 0
+    for q, (expected, *input_args) in enumerate(cases):
+        result = TEST_CALL(*input_args)
+        if result == expected:
+            print(f"{q}: passed")
+        else:
+            print(f"{q}: FAILED")
+            print(f"  {expected} != {result}")
+            failed += 1
+    if failed:
+        print(f"FAILED: {failed}")
+    else:
+        print(f"SUCCESS: TESTS PASSED == {len(cases)}")
+
+    return failed
+
+
+if __name__ == "__main__":
+    import sys
+    sys.exit(test())
